3ca30f0461f8abb3910040dc766d595a9af2cba0
Romuald Brillout <github.com@brillout.com>
2016-07-28 14:26:13 +0200

    initial commit

---
+++ dist/index.es5.js
@@ -0,0 +1,56 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _extendableBuiltin(cls) {
+    function ExtendableBuiltin() {
+        var instance = Reflect.construct(cls, Array.from(arguments));
+        Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+        return instance;
+    }
+
+    ExtendableBuiltin.prototype = Object.create(cls.prototype, {
+        constructor: {
+            value: cls,
+            enumerable: false,
+            writable: true,
+            configurable: true
+        }
+    });
+
+    if (Object.setPrototypeOf) {
+        Object.setPrototypeOf(ExtendableBuiltin, cls);
+    } else {
+        ExtendableBuiltin.__proto__ = cls;
+    }
+
+    return ExtendableBuiltin;
+}
+
+var ExtendableError = function (_extendableBuiltin2) {
+    _inherits(ExtendableError, _extendableBuiltin2);
+
+    function ExtendableError(message) {
+        _classCallCheck(this, ExtendableError);
+
+        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(ExtendableError).call(this, message));
+
+        _this.name = _this.constructor.name;
+        _this.message = message;
+        if (typeof Error.captureStackTrace === 'function') {
+            Error.captureStackTrace(_this, _this.constructor);
+        } else {
+            _this.stack = new Error(message).stack;
+        }
+        return _this;
+    }
+
+    return ExtendableError;
+}(_extendableBuiltin(Error));
+
+module.exports = ExtendableError;
+
---
+++ readme.md
@@ -0,0 +1,48 @@
+#### Usage
+
+```js
+const ExtendableError = require('extendable-error-class');
+
+class MyError extends ExtendableError {
+    constructor(m) {
+        super(m);
+    }
+}
+
+```
+
+
+
+### Why
+
+Because the following doesn't work as expected.
+```js
+class MyError extends Error {
+    constructor(m) {
+        super(m);
+    }
+}
+```
+
+
+
+### How
+
+The workaround is based on `babel-plugin-transform-builtin-extend` and following code
+
+```js
+const ExtendableError = class extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+        this.message = message;
+        if (typeof Error.captureStackTrace === 'function') {
+            Error.captureStackTrace(this, this.constructor);
+        } else {
+            this.stack = (new Error(message)).stack;
+        }
+    }
+};
+```
+
+See http://stackoverflow.com/questions/31089801/extending-error-in-javascript-with-es6-syntax
